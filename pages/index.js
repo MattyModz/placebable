032524988a7af0f5084011cac84ef545020c5b34
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-17 21:01:01 +0100

    blog

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Container from "../src/componants/container";
 import Hero from "../src/componants/Hero";
 import Sectionone from "../src/componants/sectionone";
 import Sectiontwo from "../src/componants/sectiontwo";
-import Sectionthree from "../src/componants/sectionthree";
+// import Sectionthree from "../src/componants/sectionthree";
 export default function Home() {
   return (
     <>
