7d507b462364dc7173df01fcffd4a35cab7e8371
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-23 17:47:32 +0100

    initial commit

--- src/componants/sectionone/index.js
+++ src/componants/sectionone/index.js
@@ -2,39 +2,41 @@ import React from "react";
 
 function Sectionone() {
   return (
-    <div className="lg:h-screen h-full bg-[#FFF6E9] grid items-center ">
-      <div className="flex ">
-        <div className="lg:w-1/3"></div>
-        <div className="lg:w-1/3  ">
-          <div className="text-center  ">
-            <div className="font-bold text-3xl">Joey and Toby</div>
-            <div className="font-bold text-3xl">Placeable, Co-Founders</div>
-            <br></br>
-            <br></br>
-            <div className="text-center text-xl">
-              “You can’t. Businesses and candidates are people, not numbers.
-              Great recruitment can never be a transaction.
-              <br />
-              <br /> And that’s why we founded Placeable. <br />
-              <br />
-              Every business, every person, has potential. If things line up, We
-              know you’ll fly. <br />
-              <br />
-              We listen. We analyse. We compare. Because if we don’t, what value
-              are we really adding? <br />
-              <br />
-              We work to find only the candidates and opportunities that are
-              specifically suited to you. Because whether you’re building a team
-              or joining one, good enough shouldn’t ever cut it.
-              <br />
-              <br />
-            </div>
-            {/* <div className="text-center">
+    <div className="bg-[#FFF6E9]">
+      <div className="lg:h-screen h-full bg-[#FFF6E9] grid items-center container ">
+        <div className="flex ">
+          <div className="lg:w-1/3"></div>
+          <div className="lg:w-1/3  ">
+            <div className="text-center  ">
+              <div className="font-bold text-3xl">Joey and Toby</div>
+              <div className="font-bold text-3xl">Placeable, Co-Founders</div>
+              <br></br>
+              <br></br>
+              <div className="text-center text-xl">
+                “You can’t. Businesses and candidates are people, not numbers.
+                Great recruitment can never be a transaction.
+                <br />
+                <br /> And that’s why we founded Placeable. <br />
+                <br />
+                Every business, every person, has potential. If things line up,
+                We know you’ll fly. <br />
+                <br />
+                We listen. We analyse. We compare. Because if we don’t, what
+                value are we really adding? <br />
+                <br />
+                We work to find only the candidates and opportunities that are
+                specifically suited to you. Because whether you’re building a
+                team or joining one, good enough shouldn’t ever cut it.
+                <br />
+                <br />
+              </div>
+              {/* <div className="text-center">
               Find out how we're doing things differently
             </div> */}
+            </div>
           </div>
+          <div className="lg:w-1/3"></div>
         </div>
-        <div className="lg:w-1/3"></div>
       </div>
     </div>
   );
