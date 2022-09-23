0f1f8f87c73f9fc0b8320d5736887e07c44d3ff7
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-23 17:42:24 +0100

    initial commit

--- src/componants/sectionfour/index.js
+++ src/componants/sectionfour/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { RoughNotation } from "react-rough-notation";
 function Sectionfour() {
   return (
-    <div className="bg-[#FFF6E9] lg:h-screen">
+    <div className="bg-[#FFF6E9] lg:h-screen h-full container">
       <div className=" relative h-full container  lg:bg-p2topbg bg-no-repeat bg-[#FFF6E9] ">
         <div className="container  p-4">
           <div className="lg:flex  container">
