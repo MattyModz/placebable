a967464d4286c7059de682f9df6431492b008e95
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-25 17:38:20 +0100

    section 2 height

--- src/componants/sectionfour/index.js
+++ src/componants/sectionfour/index.js
@@ -4,7 +4,7 @@ import Card from "./card";
 function Sectionfour() {
   return (
     <section className="bg-[#FFF6E9] h-full ">
-      <div className="container 2xl:bg-p2topbg xl:bg-p2topbg bg-no-repeat xl:bg-contain 2xl:bg-cover   ">
+      <div className="container 2xl:bg-p2topbg xl:bg-p2topbg bg-no-repeat bg-contain    ">
         <div className="xl:flex lg:h-full xl:h-screen 2xl:h-screen border">
           <div className="2xl:w-2/3 xl:w-1/2 lg:w-1/1 border">
             <div className="2xl:flex   ">
