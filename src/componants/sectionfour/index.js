6a41857e307fb66ad28f775a737a30a25505167b
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-25 17:02:01 +0100

    section 2 height

--- src/componants/sectionfour/index.js
+++ src/componants/sectionfour/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RoughNotation } from "react-rough-notation";
+// import { RoughNotation } from "react-rough-notation";
 import Card from "./card";
 function Sectionfour() {
   return (
