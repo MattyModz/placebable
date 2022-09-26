f58f7495a24bc3d4720cef8d8c1e22939c523b3c
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-26 09:01:09 +0100

    section 2 height

--- src/componants/sectionone/index.js
+++ src/componants/sectionone/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Sectionone() {
   return (
-    <div className="bg-[#FFF6E9] h-screen">
+    <div className="bg-[#FFF6E9] h-full ">
       <div className="lg:h-screen h-full bg-[#FFF6E9] grid items-center container ">
         <div className="flex ">
           <div className="lg:w-1/4"></div>
