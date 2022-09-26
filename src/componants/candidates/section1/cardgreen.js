8822b359b6fd34b05aebe1bac67506e87078d47b
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-09-26 08:50:56 +0100

    section 2 height

--- src/componants/candidates/section1/cardgreen.js
+++ src/componants/candidates/section1/cardgreen.js
@@ -5,15 +5,15 @@ function Cardgreen({ role, name }) {
     <div className="bg-[#8CD87C] p-8 m-4 rounded-xl">
       <p className="p-8">
         {" "}
-        “They took the time to really understand me as an individual and what I
-        was looking for in my next move, this went beyond surface level trying
-        to sell me a role and meant we had a great understanding of what
+        &lsquo;They took the time to really understand me as an individual and
+        what I was looking for in my next move, this went beyond surface level
+        trying to sell me a role and meant we had a great understanding of what
         mattered early on. Joey went above and beyond helping me both with
         preparation but also as an emotional support, which every candidate
-        could do with when they really love the idea of a role. I can't
+        could do with when they really love the idea of a role. I can&quot;t
         recommend Joey and Toby enough from start to finish they have gone above
         and beyond. It&quot;s the first time I&quot;ve ever felt like a
-        recruiter truly has my best interest at heart.”
+        recruiter truly has my best interest at heart.&lsquo;
       </p>
       <h3 className="py-4 text-2xl font-bold">
         {role} | <br />
