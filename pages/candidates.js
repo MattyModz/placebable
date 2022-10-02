877e732fa10ad4062cc93ebfab6b5bfdfe625099
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-02 14:58:35 +0100

    footer height

--- pages/candidates.js
+++ pages/candidates.js
@@ -2,7 +2,7 @@ import React from "react";
 import Indexcandid from "../src/componants/Hero/indexcandid";
 import Contactform from "../src/componants/Contact";
 import Sectionfive from "../src/componants/clients/sectionfive";
-import Sectionsix from "../src/componants/clients/sectionsix";
+
 import S1candid from "../src/componants/candidates/section1";
 import Candidhow from "../src/componants/candidates/section3";
 function Candidates() {
