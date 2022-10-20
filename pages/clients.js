4a81b712eec887cf6509d9a645038d3398da32fa
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-20 16:39:36 +0100

    url slugs and sitemap

--- pages/clients.js
+++ pages/clients.js
@@ -6,7 +6,7 @@ import Sectionsix from "../src/componants/clients/sectionsix";
 import Contactform from "../src/componants/Contact/index";
 function Clients() {
   return (
-    <div>
+    <div className="">
       <div id="1">
         {" "}
         <Indexclient />
@@ -19,7 +19,7 @@ function Clients() {
         {" "}
         <Sectionfive />
       </div>
-      <div id="4">
+      <div id="4" className="bg-[#1E1E1E] h-screen grid items-center">
         {" "}
         <Sectionsix />{" "}
       </div>
