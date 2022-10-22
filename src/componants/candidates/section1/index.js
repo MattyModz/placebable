e4b2bc11ec7a3332e56ecdffccb70d5dc4d7520b
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-22 16:37:17 +0100

    url slugs

--- src/componants/candidates/section1/index.js
+++ src/componants/candidates/section1/index.js
@@ -5,13 +5,36 @@ import Image from "next/image";
 function S1candid() {
   return (
     <section className="  bg-[#FFF6E9]  lg:p-12 pb-8">
-      <div className="grid items-center  ">
-        <div className="flex justify-center ">
+      <div className="grid   ">
+        <div className="lg:flex justify-center hidden   ">
           <Image src={"/p3/say/bgasset.svg"} width={1200} height={700} />
-          <div className="absolute border w-full flex justify-end px-4 container ">
+          <div className="absolute border w-full lg:flex justify-end px-4 container hidden  ">
             {" "}
-            <Image src={"/p3/say/title.svg"} width={500} height={400} />
+            <Image src={"/p3/say/title.svg"} width={500} height={250} />
           </div>
+        </div>
+      </div>
+      <div className="  hidden sm:block flex justify-center">
+        <div className=" hidden md:block sm:hidden lg:hidden">
+          <div className=" border w-full flex justify-end  px-4 container   ">
+            {" "}
+            <Image src={"/p3/say/title.svg"} width={500} height={250} />
+          </div>
+          <div className="flex justify-center">
+            <Image src={"/p3/say/bgassetmd.svg"} width={900} height={400} />
+          </div>
+        </div>
+      </div>
+      <div className=" border w-full flex justify-end  px-4 container  md:hidden  ">
+        {" "}
+        <Image src={"/p3/say/title.svg"} width={500} height={250} />
+      </div>
+      <div className="h-full     justify-center md:hidden bg-p2bgassetsmsay pb-24 bg-no-repeat bg-contain container bg-center ">
+        {" "}
+        <div className=" py-8 ">
+          {" "}
+          <Image src={"/p3/say/orange.svg"} width={500} height={500} />
+          <Image src={"/p3/say/green.svg"} width={500} height={500} />
         </div>
       </div>
     </section>
