a93fc7d7cb4cefacd1ff9343e9c7070e2b5ab2b9
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-28 15:21:02 +0100

    headless ui react update

--- src/componants/Hero/index.js
+++ src/componants/Hero/index.js
@@ -9,58 +9,61 @@ function Hero() {
   return (
     <>
       <header className="relative flex items-center justify-center w-full      overflow-hidden px-4">
-        <div className="relative w-full z-30    ">
+        <div className="relative w-full z-30 h-screen   ">
           <Nav />
-          <section className=" container grid grid-rows-2 ">
-            <div className=" 2xl:text-5xl xl:text-5xl lg:4xl text-4xl">
-              <div className=" font-bold text-[#FFF6E9] ">Recruitment 2.0</div>
-              <div className="py-4 -ml-4">
-                <Image
-                  src={"/lines/index/thejob.svg"}
-                  width={450}
-                  height={220}
-                />
-              </div>
-            </div>
-
-            <div className="flex justify-end   ">
-              {" "}
-              <Image src={"/lines/index/sub.svg"} width={450} height={220} />
-            </div>
-            <div className="lg:flex    container">
-              <div className="lg:w-1/3">
-                {" "}
-                <div className="">
-                  {" "}
-                  <a href="#1">
-                    <img src={"./S1Arrow.svg"} width={50} height={100} />
-                  </a>
+          <section className=" container   h-full ">
+            <div className="   container h-full grid items-stretch">
+              <div className=" 2xl:text-5xl xl:text-5xl lg:4xl text-4xl">
+                <div className=" font-bold text-[#FFF6E9] ">
+                  Recruitment 2.0
                 </div>
-                <div className="  text-xl">
-                  <div className="w-1/2 py-4 text-[#FFF6E9] underline">
+                <div className=" -ml-4">
+                  <Image
+                    src={"/lines/index/thejob.svg"}
+                    width={450}
+                    height={220}
+                  />
+                </div>
+              </div>
+              <div className="  flex justify-end  ">
+                {" "}
+                <Image src={"/lines/index/sub.svg"} width={450} height={50} />
+              </div>
+              <div className="lg:flex lg:-mt-8">
+                <div className="lg:w-1/3">
+                  {" "}
+                  <div className="">
                     {" "}
                     <a href="#1">
-                      Discover why its time to give Placeable a shot
+                      <img src={"./S1Arrow.svg"} width={50} height={100} />
                     </a>
                   </div>
+                  <div className="  text-xl">
+                    <div className="w-1/2  text-[#FFF6E9] underline">
+                      {" "}
+                      <a href="#1">
+                        Discover why its time to give Placeable a shot
+                      </a>
+                    </div>
+                  </div>
                 </div>
+                <div className="lg:w-1/3  justify-center">
+                  <div className="flex justify-center">
+                    {" "}
+                    <img src={"./S1play.svg"} width={50} height={50} />
+                  </div>
+                  <div
+                    className="flex underline justify-center text-xl  text-[#FFF6E9]"
+                    onClick={() => {
+                      setShowModal(true);
+                    }}
+                  >
+                    {" "}
+                    Watch the full video
+                  </div>
+                </div>
+                <div className="lg:w-1/3"></div>
               </div>
-              <div className="lg:w-1/3  justify-center">
-                <div className="flex justify-center">
-                  {" "}
-                  <img src={"./S1play.svg"} width={50} height={50} />
-                </div>
-                <div
-                  className="flex underline justify-center text-xl py-6 text-[#FFF6E9]"
-                  onClick={() => {
-                    setShowModal(true);
-                  }}
-                >
-                  {" "}
-                  Watch the full video
-                </div>
-              </div>
-              <div className="lg:w-1/3"></div>
             </div>
           </section>
         </div>
@@ -69,7 +72,7 @@ function Hero() {
           autoPlay
           loop
           muted
-          class="absolute z-10 absolute z-10 w-auto w-full "
+          class="absolute z-10 w-auto min-w-full  max-w-none "
         >
           <source src="/Landing page.mp4" type="video/mp4" />
           Your browser does not support the video tag.
