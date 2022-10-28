657ab19ca11b891103fbefc085b070d28db8d4c9
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-10-28 15:02:35 +0100

    headless ui react update

--- src/componants/Hero/indexcandid.js
+++ src/componants/Hero/indexcandid.js
@@ -8,48 +8,46 @@ function Indexcandid() {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
-      <header className="relative flex items-center  justify-center w-full overflow-hidden px-4 bg-black   ">
-        <div className=" relative w-full z-30   h-full ">
+      <header className="relative flex items-center  justify-center w-full  overflow-hidden px-4 bg-black   ">
+        <div className=" relative w-full z-30 h-screen   ">
           <Navcandid />
-          <section className="container   ">
-            <div className="grid grid-rows-2 ">
-              <div className="   lg:w-1/2 justify-start flex ">
-                <Image src={"/p3/hero/head.svg"} width={900} height={500} />
-              </div>
-              <div className="  w-full  items-center  py-8  lg:flex">
+          <section className="container    h-full  items-stretch grid">
+            <div className="   lg:w-1/2 justify-start flex ">
+              <Image src={"/p3/hero/head.svg"} width={900} height={500} />
+            </div>
+            <div className="  w-full      lg:flex">
+              {" "}
+              <div className="lg:w-1/3">
                 {" "}
-                <div className="lg:w-1/3">
+                <div className="">
                   {" "}
-                  <div className="">
+                  <a href="#2">
+                    <img src={"./S1Arrow.svg"} width={50} height={50} />
+                  </a>
+                </div>
+                <div className="  text-xl">
+                  <div className="w-1/2   text-[#FFF6E9] underline">
                     {" "}
-                    <a href="#2">
-                      <img src={"./S1Arrow.svg"} width={50} height={50} />
-                    </a>
-                  </div>
-                  <div className="  text-xl">
-                    <div className="w-1/2   text-[#FFF6E9] underline">
-                      {" "}
-                      <a href="#2">Discover Placeable</a>
-                    </div>
+                    <a href="#2">Discover Placeable</a>
                   </div>
                 </div>
-                <div className="lg:w-1/3  justify-center">
-                  <div className="flex justify-center">
-                    {" "}
-                    <img src={"./S1play.svg"} width={50} height={50} />
-                  </div>
-                  <div
-                    className="flex underline justify-center text-xl py-6 text-[#FFF6E9]"
-                    onClick={() => {
-                      setShowModal(true);
-                    }}
-                  >
-                    {" "}
-                    Watch the full video
-                  </div>
-                </div>
-                <div className="lg:w-1/3"></div>
               </div>
+              <div className="lg:w-1/3  justify-center">
+                <div className="flex justify-center">
+                  {" "}
+                  <img src={"./S1play.svg"} width={50} height={50} />
+                </div>
+                <div
+                  className="flex underline justify-center text-xl py-6 text-[#FFF6E9]"
+                  onClick={() => {
+                    setShowModal(true);
+                  }}
+                >
+                  {" "}
+                  Watch the full video
+                </div>
+              </div>
+              <div className="lg:w-1/3"></div>
             </div>
           </section>
         </div>
