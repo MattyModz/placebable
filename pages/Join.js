845239123b10fee33717edcd91673e8c6fc12978
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-11-04 10:10:48 +0000

    url updates

--- pages/Join.js
+++ pages/Join.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import Nav from "../Header/nav";
 // import Modal from "../Modal/Modal";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 function indexjoin() {
   return (
